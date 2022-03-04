aefdf2df5abf772a0412f8a92f1f769e8be569e9
nikhilbpatel <nikhilbpatel2002@gmail.com>
2022-03-04 18:20:03 +0530

    profile update

--- client/src/Component/Profile/Profile.jsx
+++ client/src/Component/Profile/Profile.jsx
@@ -39,7 +39,7 @@ export default function Profile() {
       .catch((err) => {
         console.log(err);
       });
-  }, []);
+  }, [flag]);
   function handleChange(e) {
     const { name, value } = e.target;
     setProfile({
